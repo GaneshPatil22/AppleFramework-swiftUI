f4ad9cbc17f655db77c1113d43d2b8403710385c
GaneshPatil22 <ganeshbuddy.patil@gmail.com>
2024-12-09 19:11:09 +0530

    Initial commit

---
+++ AppleFramework-SwiftUI/AppleFramework_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFramework_SwiftUIApp.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by Ganesh Patil on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFramework_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
